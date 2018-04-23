7a36c86b6c106a665eedab268874b77111774c90
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BaseEdit_BorderTemplate/Window1.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace BaseEdit_BorderTemplate {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Customize the Editor's Border via Templates
+
+
+<p>The following example shows how to customize the editor's border via templates.</p>
+
+<br/>
+
+
