15f2e71f686e43479b2409fca0fa531bdf40486c
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-16 18:16:18 +0400

    Change example status to out of maintenance

--- CS/BaseEdit_BorderTemplate/Window1.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace BaseEdit_BorderTemplate {
-    public partial class Window1 : Window {
-        public Window1() {
-            InitializeComponent();
-        }
-    }
-}
